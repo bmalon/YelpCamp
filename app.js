5eaed83a3f518833807774d2ebd834d042c41dc2
bmalon <bartlomiejmalon94@gmail.com>
2022-02-24 14:02:13 +0100

    corrections to app.js to meet ESLint Airbnb style

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('WELCOME TO YelpCamp');
-})
+});
 
 app.listen(3000, () => {
   console.log('Serving on port 3000');
-})
-
-
+});
