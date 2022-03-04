0394e762786880fe644de1ae502519c456420145
bmalon <bartlomiejmalon94@gmail.com>
2022-03-04 18:28:34 +0100

    added basic review creation functionality

--- app.mjs
+++ app.mjs
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url';
 import morgan from 'morgan';
 import methodOverride from 'method-override';
 import Campground from './models/campground.mjs';
+import Review from './models/review.mjs';
 import CampgroundSchema from './schemas.mjs';
 import ExpressError from './utils/ExpressError.mjs';
 import catchAsync from './utils/catchAsync.mjs';
@@ -88,6 +89,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
+}));
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 app.all('*', (req, res, next) => {
