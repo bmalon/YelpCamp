fc5a34c3798f7a396056693a88cbef3b5c7df47c
bmalon <bartlomiejmalon94@gmail.com>
2022-03-14 10:17:57 +0100

    fixed the register route - user is logged in immediately after registering

--- routes/users.mjs
+++ routes/users.mjs
@@ -11,13 +11,16 @@ UserRouter.get('/register', (req, res) => {
   res.render('users/register');
 });
 
-UserRouter.post('/register', catchAsync(async (req, res) => {
+UserRouter.post('/register', catchAsync(async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const user = new User({ username, email });
     const registeredUser = await User.register(user, password);
-    req.flash('success', 'Welcome to YelpCamp!');
-    res.redirect('/campgrounds');
+    req.login(registeredUser, (err) => {
+      if (err) next(err);
+      req.flash('success', 'Welcome to YelpCamp!');
+      res.redirect('/campgrounds');
+    });
   } catch (e) {
     req.flash('error', e.message);
     res.redirect('register');
