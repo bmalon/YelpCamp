dc156bda11070e31f780c9821d75bd7aca488d93
bmalon <bartlomiejmalon94@gmail.com>
2022-03-16 12:17:35 +0100

    fixed CloudinaryStorage config

--- cloudinary/index.mjs
+++ cloudinary/index.mjs
@@ -9,8 +9,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  folder: 'YelpCamp',
-  allowedFormats: ['jpeg', 'jpg', 'png'],
+  params: {
+    folder: 'YelpCamp',
+    allowedFormats: ['jpeg', 'jpg', 'png'],
+  },
 });
 
 export {
